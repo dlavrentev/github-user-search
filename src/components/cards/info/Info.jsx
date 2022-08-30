9f45f43d38db0329322aa4d1090bb067f7c88cc2
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-30 13:11:40 +0200

    Update Info.jsx

--- src/components/cards/info/Info.jsx
+++ src/components/cards/info/Info.jsx
@@ -1,15 +1,45 @@
 import React from "react";
 import Image from "./../../../assets/profile.jpeg";
 
+import Location from "./../../../assets/icon-location.svg";
+import Website from "./../../../assets/icon-website.svg";
+import Twitter from "./../../../assets/icon-twitter.svg";
+import Company from "./../../../assets/icon-company.svg";
+
 const Info = () => {
   return (
     <div className="info-container">
-      <div>
+      <header>
         <img src={Image} alt="Profile" />
         <div>
           <h3>THe Octocat</h3>
           <h4>@octocat</h4>
           <h4>Joined 25 Jan 2011</h4>
+        </div>
+      </header>
+      <div>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+          Quisque volutpat mattis eros.
+        </h4>
+        <div>REPOS</div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <h4>San Francisco</h4>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>San Francisco</h4>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>San Francisco</h4>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <h4>San Francisco</h4>
+          </div>
         </div>
       </div>
     </div>
