52ed696dc49cfc8e06f77ce9476b375e67854808
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-30 13:03:12 +0200

    updated info.jsx

--- src/components/cards/info/Info.jsx
+++ src/components/cards/info/Info.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "./../../../assets/profile.jpeg";
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <div className="info-container">
+      <div>
+        <img src={Image} alt="Profile" />
+        <div>
+          <h3>THe Octocat</h3>
+          <h4>@octocat</h4>
+          <h4>Joined 25 Jan 2011</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
