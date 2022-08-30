40ac546515f3eab5be16de1a5ac882efdfc16108
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-30 12:52:40 +0200

    styling header and searchbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.scss';
-import Header from './components/header/Header';
-import Searchbar from './components/searchbar/Searchbar';
+import "./App.scss";
+import Header from "./components/header/Header";
+import Searchbar from "./components/searchbar/Searchbar";
+import Info from "./components/cards/info/Info";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Searchbar />
+      <Info />
     </div>
   );
 }
