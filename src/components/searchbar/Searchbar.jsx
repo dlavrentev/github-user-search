dfb5d4125658a9001c67fa757bee64500a71e986
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-31 18:20:46 +0200

    updated

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./../searchbar/_searchbar.scss";
+import "./../searchbar/_searchbar-tablet.scss";
 import Search from "./../../assets/icon-search.svg";
 
 const Searchbar = () => {
